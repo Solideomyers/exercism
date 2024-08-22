f5f1aed36a81809c1bfb320f9fe6eca88bd02c8c
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-08-22 16:17:22 -0400

    feat: concepts regex done

--- regular-chatbot/regular-chatbot.js
+++ regular-chatbot/regular-chatbot.js
@@ -7,8 +7,13 @@
  * @returns {boolean} whether or not is the command valid
  */
 
+// Use the test() method for returning a boolean.
+// Remember that we can use flags at the end of the regular expression for additional features such as case insensitive.
 export function isValidCommand(command) {
-  throw new Error('Please implement the isValidCommand function');
+  const commandRegex = /^chatbot,\s/i;
+
+  return commandRegex.test(command);
+
 }
 
 /**
@@ -17,8 +22,20 @@ export function isValidCommand(command) {
  * @param {string} message
  * @returns {string} The message without the emojis encryption
  */
+
+// In this case we must use the RexExp constructor to build our regular expression.
+// Thanks to the common encryption of each emoji, we can use [0-9] to search for any digit after the emojiword.
+// The character + matches one or more consecutive items.
+// Use the split() method alongside the regex for each emoji to get rid of all encryption.
+// Finally, return this splitted array as a string using the join() method.
+
 export function removeEmoji(message) {
-  throw new Error('Please implement the removeEmoji function');
+
+  const emojiRegex = /emoji[0-9]+/g;
+
+  return message.split(emojiRegex).join('')
+  
+
 }
 
 /**
@@ -27,8 +44,23 @@ export function removeEmoji(message) {
  * @param {string} number
  * @returns {string} the Chatbot response to the phone Validation
  */
+
+// In this exercise we are playing with groups and ranges. Read this documentation for learning more about it.
+// This article is really good at explaining different ways of number validation.
+// You may also want to use this cheatsheet for a broad view of available features in regular expressions.
+// Use the test() method to check whether the phone number is valid or not.
+// Return the final answer with a simple if statement.
+
 export function checkPhoneNumber(number) {
-  throw new Error('Please implement the checkPhoneNumber function');
+const phoneRegex = /^\(\+\d{2}\)\s\d{3}-\d{3}-\d{3}$/;
+
+const isValid = phoneRegex.test(number);
+
+if (isValid) {
+  return 'Thanks! You can now download me to your phone.';
+} else {
+  return `Oops, it seems like I can't reach out to ${number}`;
+}
 }
 
 /**
@@ -37,8 +69,17 @@ export function checkPhoneNumber(number) {
  * @param {string} userInput
  * @returns {string[] | null} all the possible URL's that the user may have answered
  */
+
+// We are targeting words that are joined by one or more .(dots).
+// Remember to put the flag g at the end so we can get all the domains in any sentence.
+// Use the match() method to return an array with all the previous matches.
+
 export function getURL(userInput) {
-  throw new Error('Please implement the userInput function');
+  const urlRegex = /\b(?:[a-z]+\.)?[a-z]+\.[a-z]{2,6}\b/g;
+
+  const urls = userInput.match(urlRegex);
+
+  return urls || null;
 }
 
 /**
@@ -47,6 +88,20 @@ export function getURL(userInput) {
  * @param {string} fullName
  * @returns {string} Greeting from the chatbot
  */
+
+// First, use the replace() method directly to the given string.
+// Use Named Capture Groups to store the name and surname into different values.
+// Pass a (...match) destructuring to the replacer function so you can get the name and surname into the object within the matched groups.
+// Use the pop() method to store the matched object with name and surname.
+// Finally, implement a template String so you can get the values from the regular expression object. See this example for knowing about how match() stores objects.
 export function niceToMeetYou(fullName) {
-  throw new Error('Please implement the fullName function');
+ const namePattern = /(?<lastName>[a-zA-Z]+),\s+(?<firstName>[a-zA-Z]+)/;
+ const match = fullName.match(namePattern);
+
+ if (match && match.groups) {
+   const { firstName, lastName } = match.groups;
+   return `Nice to meet you, ${firstName} ${lastName}`;
+ }
+
+ return `Nice to meet you!`;
 }
